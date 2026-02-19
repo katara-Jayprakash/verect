a3b41eca119219d0111fcf594bbcf154eb844478
katara-Jayprakash <katarajayprakash@icloud.com>
2026-02-19 13:38:56 +0530

    introducing try-catch-block
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- build-server/script.js
+++ build-server/script.js
@@ -24,36 +24,40 @@ const S3client = new S3Client({
 async function main() {
   console.log("executing script.js file");
   const outerDirPath = path.join(__dirname, "output");
-  const cmd = exec(
-    `cd ${outerDirPath} && npm install -g pnpm && pnpm install && pnpm run build`,
-  );
-  cmd.stdout.on("data", (data) => {
-    console.log("data", data.toString());
-  });
-  cmd.stderr.on("error", (error) => {
-    console.log("error", error.toString());
-  });
-  cmd.on("close", async function () {
-    console.log("build complete");
-    const distOutputFolder = path.join(__dirname, "output", "dist");
-    /*we need to read the files synchronously, to make sure every file is there
-    going to give me return of array */
-    const distContent = fs.readdirSync(distOutputFolder, { recursive: true });
-    for (const filepath of distContent) {
-      if (fs.lstatSync(filepath).isDirectory()) continue; // removing folder from filepath,
-      // putting things into s3 bucket
-      const input = {
-        Bucket: BUCKET_NAME,
-        //its means how you going to store things into s3-(folderName),
-        Key: `__outputs/${PROJECT_ID}/${filepath}`,
-        // what going to be store into keys or folder
-        Body: fs.createReadStream(filepath),
-        // dynamic telling the s3 that content can be anything with using mime,
-        ContentType: mime.lookup(`${filepath}`),
-      };
-      const command = new PutObjectCommand(input);
-      const response = await S3client.send(command);
-      console.log(response);
-    }
-  });
+  try {
+    const cmd = exec(
+      `cd ${outerDirPath} && npm install -g pnpm && pnpm install && pnpm run build`,
+    );
+    cmd.stdout.on("data", (data) => {
+      console.log("data", data.toString());
+    });
+    cmd.stderr.on("error", (error) => {
+      console.log("error", error.toString());
+    });
+    cmd.on("close", async function () {
+      console.log("build complete");
+      const distOutputFolder = path.join(__dirname, "output", "dist");
+      /*we need to read the files synchronously, to make sure every file is there
+      going to give me return of array */
+      const distContent = fs.readdirSync(distOutputFolder, { recursive: true });
+      for (const filepath of distContent) {
+        if (fs.lstatSync(filepath).isDirectory()) continue; // removing folder from filepath,
+        // putting things into s3 bucket
+        const input = {
+          Bucket: BUCKET_NAME,
+          //its means how you going to store things into s3-(folderName),
+          Key: `__outputs/${PROJECT_ID}/${filepath}`,
+          // what going to be store into keys or folder
+          Body: fs.createReadStream(filepath),
+          // dynamic telling the s3 that content can be anything with using mime,
+          ContentType: mime.lookup(`${filepath}`),
+        };
+        const command = new PutObjectCommand(input);
+        const response = await S3client.send(command);
+        console.log(response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
