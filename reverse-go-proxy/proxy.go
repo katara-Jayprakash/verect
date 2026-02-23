fe7601027d8a58782cc11a47632812ddfcff4943
katara-Jayprakash <katarajayprakash@icloud.com>
2026-02-23 20:53:44 +0530

    try to build proxy.go
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- reverse-go-proxy/proxy.go
+++ reverse-go-proxy/proxy.go
@@ -10,53 +10,46 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"sync"
 )
 
-var mu sync.RWMutex
-var Port string = ":3000"
-var database map[int]task
-
-type task struct {
-	Id        int    `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
+type Response struct {
+	Message string `json:"message"`
+	Method  string `json:"method"`
+	Host    string `json:"host"`
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		tasklist := []task{}
-		mu.RLock() //locking for safety purpose
-		for _, t := range database {
-			tasklist = append(tasklist, t)
-		}
-		mu.RUnlock() //unlocking thread
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(tasklist)
-	}
-
-	if r.Method == http.MethodPost {
-		nextId := len(database) + 1
-		var newTask task
-		json.NewDecoder(r.Body).Decode(&newTask)
-		mu.Lock()
-		newTask.Id = nextId
-		database[nextId] = newTask
-
-		mu.Unlock()
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("server sent you message!"))
-	}
+	fmt.Println(r.Method)
+	fmt.Println(r.URL.String())
+	fmt.Fprintf(w, "server got your message")
 
 }
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "health route is ready")
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.Header().Set("Content-Type", "application/json")
+		response := Response{
+			Message: "hi got your message thanx client",
+			Method:  r.Method,
+			Host:    r.Host,
+		}
+		json.NewEncoder(w).Encode(response)
+
+	}
+}
 func main() {
-	database = make(map[int]task)
+	Port := ":80"
+	mux := http.NewServeMux()
 
-	// http server
-	http.HandleFunc("/", handleRequest)
-	fmt.Println("server is running on port" + Port)
+	fmt.Println("server is running on Port:", Port)
+	mux.HandleFunc("/", handleRequest)
+	mux.HandleFunc("/health", health)
+	mux.HandleFunc("/api", handler)
 
-	http.ListenAndServe(Port, nil)
+	log.Fatal(http.ListenAndServe(Port, mux))
 
 }
