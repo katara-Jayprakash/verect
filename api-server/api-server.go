997c4c6b1570d3f291dded14bdb0b615bf97f1c8
katara-Jayprakash <katarajayprakash@icloud.com>
2026-03-30 00:22:35 +0530

    added config file lhat was actually prank
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- api-server/api-server.go
+++ api-server/api-server.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "health route is ready")
+}
+func init() {
+	// load variable from the .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	if kubeConfigBase64 := os.Getenv("KUBECONFIG_BASE64"); kubeConfigBase64 != "" {
+		// if kubeConfigBase exist then decode its and use it;
+		// and if its does not exist then try to find out locally config with /.kube/config
+		data := make([]byte, base64.StdEncoding.DecodedLen(len(kubeConfigBase64)))
+		n, err := base64.StdEncoding.Decode(data, []byte(kubeConfigBase64))
+
+		// fmt.Println(kubeConfigBase64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(data[:n]))
+
+	}
+
 }
 func main() {
 	Port := ":9000"
--- api-server/go.mod
+++ api-server/go.mod
@@ -2,4 +2,4 @@ module github.com/katara-Jayprakash/verect
 
 go 1.26.1
 
-require k8s.io/client-go v0.31.0 // indirect
+require github.com/joho/godotenv v1.5.1
