54b46b710b38cda786ca1d51ca7486778e26a4e7
katara-Jayprakash <katarajayprakash@icloud.com>
2026-03-30 09:06:52 +0530

    Refactor code and introduce single responsibility principle
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- api-server/api-server.go
+++ api-server/api-server.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -18,58 +19,67 @@ var kubeClient *kubernetes.Clientset
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "health route is ready")
 }
+
+func getconfig() (*rest.Config, error) {
+	// if kubeConfigBase exist then decode its and use it;
+	if kubeconfigDecode := os.Getenv("KUBECONFIG_BASE64"); kubeconfigDecode != "" {
+		kubeconfigBytes, err := base64.StdEncoding.DecodeString(kubeconfigDecode)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode kubeconfig: %w", err)
+		}
+		// This is used when your kubeconfig is in memory (byte array), not a file.
+		kubeConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfigBytes)
+		if err != nil {
+			return nil, fmt.Errorf("cannot create kube config from Base64: %w", err)
+		}
+
+		log.Println("Using kubeconfig from Base64")
+		return kubeConfig, nil
+	}
+	//in-cluster config (AKS, EKS, GKE)
+	kubeConfig, err := rest.InClusterConfig()
+	if err == nil {
+		log.Println("Using in-cluster kubeconfig")
+		return kubeConfig, nil
+	}
+	// in local developement;
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("cannot find home dir: %w", err)
+	}
+	kubeConfigPath := filepath.Join(homeDir, ".kube", "config")
+	kubeConfig, err = clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot find kubeConfigdir: %w", err)
+	}
+	log.Println("Using local kubeconfig file")
+	return kubeConfig, nil
+}
 func init() {
-	/**
-	 *  load the .env file and if not present then switch to ~/.kube/config
-	 */
 	// load variable from the .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file founded shift to local .kube/config")
 	}
-
-	// if kubeConfigBase exist then decode its and use it;
-	if kubeConfigBase64 := os.Getenv("KUBECONFIG_BASE64"); kubeConfigBase64 != "" {
-		container := make([]byte, base64.StdEncoding.DecodedLen(len(kubeConfigBase64)))
-		n, err := base64.StdEncoding.Decode(container, []byte(kubeConfigBase64))
-		if err != nil {
-			log.Fatal("failed to decode kubeconfig:", err)
-		}
-		// This is used when your kubeconfig is in memory (byte array), not a file.
-		kubeConfig, err := clientcmd.RESTConfigFromKubeConfig(container[:n])
-		if err != nil {
-			log.Fatal("cannot create k8s client :", err)
-		}
-		// creating a client for interecting with k8s
-		kubeClient, err = kubernetes.NewForConfig(kubeConfig)
-		if err != nil {
-			log.Fatal("cannot create k8s client :", err)
-		}
-
-	} else {
-		currDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal("cannot find home dir:", err)
-		}
-		kubeConfigPath := filepath.Join(currDir, ".kube", "config")
-
-		// Create the client configuration from the kubeconfig file (currently we are taking configuration from <file>).
-		kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-		if err != nil {
-			fmt.Printf("Error encountered: %v\n", err)
-			return
-		}
-		// creating a client for interecting with k8s
-		kubeClient, err = kubernetes.NewForConfig(kubeConfig)
-		if err != nil {
-			log.Fatal("cannot create k8s client :", err)
-		}
+	config, err := getconfig()
+	if err != nil {
+		log.Fatal("could not get config", err)
+	}
+	kubeClient, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal("could not create k8s client", err)
 	}
 
 }
 func main() {
-	Port := ":9000"
+	Port, exists := os.LookupEnv("PORT")
+	if !exists {
+		Port = ":9000"
+	}
+	log.Println("k8s client connected successfully!")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", healthHandler)
+	// mux.HandleFunc("/")
+
 	http.ListenAndServe(Port, mux)
 }
