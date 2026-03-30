d09ce04264712bd5ede525defc31fa9684083116
katara-Jayprakash <katarajayprakash@icloud.com>
2026-03-30 08:24:41 +0530

    extracted the kubeconfig and created a client
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- api-server/api-server.go
+++ api-server/api-server.go
@@ -6,31 +6,63 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+var kubeClient *kubernetes.Clientset
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "health route is ready")
 }
 func init() {
+	/**
+	 *  load the .env file and if not present then switch to ~/.kube/config
+	 */
 	// load variable from the .env into the system
 	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
+		log.Print("No .env file founded shift to local .kube/config")
 	}
-	if kubeConfigBase64 := os.Getenv("KUBECONFIG_BASE64"); kubeConfigBase64 != "" {
-		// if kubeConfigBase exist then decode its and use it;
-		// and if its does not exist then try to find out locally config with /.kube/config
-		data := make([]byte, base64.StdEncoding.DecodedLen(len(kubeConfigBase64)))
-		n, err := base64.StdEncoding.Decode(data, []byte(kubeConfigBase64))
 
-		// fmt.Println(kubeConfigBase64)
+	// if kubeConfigBase exist then decode its and use it;
+	if kubeConfigBase64 := os.Getenv("KUBECONFIG_BASE64"); kubeConfigBase64 != "" {
+		container := make([]byte, base64.StdEncoding.DecodedLen(len(kubeConfigBase64)))
+		n, err := base64.StdEncoding.Decode(container, []byte(kubeConfigBase64))
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("failed to decode kubeconfig:", err)
+		}
+		// This is used when your kubeconfig is in memory (byte array), not a file.
+		kubeConfig, err := clientcmd.RESTConfigFromKubeConfig(container[:n])
+		if err != nil {
+			log.Fatal("cannot create k8s client :", err)
+		}
+		// creating a client for interecting with k8s
+		kubeClient, err = kubernetes.NewForConfig(kubeConfig)
+		if err != nil {
+			log.Fatal("cannot create k8s client :", err)
 		}
 
-		fmt.Println(string(data[:n]))
+	} else {
+		currDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal("cannot find home dir:", err)
+		}
+		kubeConfigPath := filepath.Join(currDir, ".kube", "config")
 
+		// Create the client configuration from the kubeconfig file (currently we are taking configuration from <file>).
+		kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		if err != nil {
+			fmt.Printf("Error encountered: %v\n", err)
+			return
+		}
+		// creating a client for interecting with k8s
+		kubeClient, err = kubernetes.NewForConfig(kubeConfig)
+		if err != nil {
+			log.Fatal("cannot create k8s client :", err)
+		}
 	}
 
 }
